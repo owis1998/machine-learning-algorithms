c959d9f892a7cf66ba79d292f47fcbce29eb4ecc
owis1998 <74670290+owis1998@users.noreply.github.com>
2021-04-05 15:12:40 +0300

    Create multiple linear regression.py

---
+++ multiple linear regression/multiple linear regression.py
@@ -0,0 +1,54 @@
+from numpy import *
+
+
+def hypothesis(inputs, t):
+	total = t[0]
+
+	for i in range(0, len(inputs)):
+		total += inputs[i] * t[i]
+		
+	return total
+
+
+def cost(points, t):
+	total = 0
+
+	for i in range(0, points.shape[0]):
+		total += (hypothesis(points[i][:-1], t) - points[i][-1]) ** 2
+
+	return total / (2 * points.shape[0])
+
+def cost_for_minize(points, t, flag):
+	total = 0
+	for i in range(0, points.shape[0]):
+		if flag:
+			total += (hypothesis(points[i][:-1], t) - points[i][-1]) * points[i][flag]
+		else:
+			total += hypothesis(points[i][:-1], t) - points[i][-1]
+
+	return total / points.shape[0]
+
+def gradient_descent(points, t, a):
+	for j in range(0, 1000):
+		tmp = []
+		for i in range(0, len(t)):
+			k = t[i] - (a * cost_for_minize(points, t, i))
+			tmp.append(k)
+		t = tmp.copy()
+
+	return t
+
+
+def main():
+	points = genfromtxt('data.csv', delimiter = ',')
+	t = [0 for _ in range(0, points.shape[1])]
+	learning_rate = 0.0001
+
+	t = gradient_descent(points, t, learning_rate)
+
+	print('the coefficients {}'.format(t))
+	print('errors {}'.format(cost(points, t)))
+
+
+if __name__ == '__main__':
+	main()
